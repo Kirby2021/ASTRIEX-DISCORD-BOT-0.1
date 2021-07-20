e7eebe85161dafedfb5f97f004e0d7c9706eeebe
vishal889 <vishalrajpurohit416@gmail.comm>
2021-07-20 14:04:09 +0530

    modified

--- commands/info/help.js
+++ commands/info/help.js
@@ -52,6 +52,7 @@ CHATBOT
 BOT-INFO
 > \`\`\`botinfo, uptime, detail \`\`\`
 
+[invite me](https://discord.com/api/oauth2/authorize?client_id=866666448269213726&permissions=8589934591&scope=bot%20applications.commands) | [support server](https://discord.gg/X7Jc6FDhFw)
 `) 
 
     await message.channel.send(embed)
